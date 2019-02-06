84c3d7e9839fc315d4801c5d45e2187099773d94
William DURAND <liam19.durand@gmail.com>
2019-02-06 21:31:59 +0100

    Modifications mineures

--- pdf_concat.py
+++ pdf_concat.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Implémenté par William DURAND
 pdf_concat: Ce script permet de parcourir tous les dossiers présents dans le dossier courant, 
 et de concaténer les pdf.
@@ -11,11 +10,9 @@ for dir in os.listdir():
     if os.path.isdir(dir):
         #Création d'une variable stockant le chemin absolu (avec nom du dossier)
         path = os.getcwd() + '\\' + dir
-
         #Permet de récupérer tous les pdf du dossier courant et de les stocker dans une liste
-        pdfs = [a for a in os.listdir(path) if a.endswith(".pdf")] 
+        pdfs = [a for a in os.listdir(path) if a.endswith(".pdf") or a.endswith(".PDF")] 
         merger = PdfFileMerger()
-
         for pdf in pdfs:
             #Création d'une variable stockant le chemin absolu (avec nom du pdf)
             pathPdf = path + '\\' + pdf
