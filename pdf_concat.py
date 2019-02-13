fd38a9192678c7af4ec4b9a3c2ebc82ce1bd07e8
William DURAND <liam19.durand@gmail.com>
2019-02-13 18:54:39 +0100

    Passage du chemin par argument

--- pdf_concat.py
+++ pdf_concat.py
@@ -4,22 +4,27 @@ et de concaténer les pdf.
 Attention ! Il est nécessaire d'installer PyPDF2 !"""
 
 import os
+import sys
 from PyPDF2 import PdfFileMerger
 
-for dir in os.listdir():
-    if os.path.isdir(dir):
+try:
+    argument = sys.argv[1]
+    for dir in os.listdir(argument):
         #Création d'une variable stockant le chemin absolu (avec nom du dossier)
-        path = os.getcwd() + '\\' + dir
-        #Permet de récupérer tous les pdf du dossier courant et de les stocker dans une liste
-        pdfs = [a for a in os.listdir(path) if a.endswith(".pdf") or a.endswith(".PDF")] 
-        merger = PdfFileMerger()
-        for pdf in pdfs:
-            #Création d'une variable stockant le chemin absolu (avec nom du pdf)
-            pathPdf = path + '\\' + pdf
-            merger.append(open(pathPdf, 'rb'))
+        path = argument + '\\' + dir
+        if os.path.isdir(path):
+            #Permet de récupérer tous les pdf du dossier courant et de les stocker dans une liste
+            pdfs = [a for a in os.listdir(path) if a.endswith(".pdf") or a.endswith(".PDF")] 
+            merger = PdfFileMerger()
+            for pdf in pdfs:
+                #Création d'une variable stockant le chemin absolu (avec nom du pdf)
+                pathPdf = path + '\\' + pdf
+                merger.append(open(pathPdf, 'rb'))
 
-        #Création d'une variable correspondant au nom du pdf produit
-        nameFile = dir + ".pdf"
+            #Création d'une variable correspondant au nom du pdf produit
+            nameFile = argument + "\\" + dir + ".pdf"
 
-        with open(nameFile, "wb") as fout:
-            merger.write(fout)
+            with open(nameFile, "wb") as fout:
+                merger.write(fout)
+except:
+    print("Aucun argument / argument non disponible")
